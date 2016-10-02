d5d110ab795908d6e60139c247c8c55d9ffc28e8
nastya <nastya-pavlova-93@yandex.ru>
2016-10-02 21:08:25 +0300

    xml_parser changed

--- context/db.php
+++ context/db.php
@@ -18,11 +18,21 @@ return $mysqli;
 }
 
 function add_place($db_conn, $data) {
-	$query = mysqli_query($db_conn, 'SELECT * FROM places WHERE lat=' . $data["lat"] . 'AND lon=' . $data["lon"]);
+	$sql = 'SELECT place_id FROM places WHERE lat=' . $data["lat"] . 'AND lon=' . $data["lon"];
+		//echo "in addd place";
+	$res = mysqli_query($db_conn, $sql);
+	if (mysqli_num_rows($res) > 0){
 
-	if (mysqli_num_rows($query) > 0){
 
-	   // echo "Place already exists";
+	//if ($db_conn->query($sql) === TRUE) {
+	    //echo "Place already exists";
+		$row = $res->fetch_assoc(); 
+		//$row = mysqli_fetch_array($res);
+		$id = $row['place_id'];
+   		//echo $id;
+		return $id;
+
+		//return $row['id'];
 	} else {
 
 	$sql = "INSERT INTO places (title, description, url, lat, lon) VALUES ('" . $data["title"] . "', '" . $data["description"] . "', '" . $data["url"] . "', '" . $data["lat"] . "', '" . $data["lon"] . "')";
@@ -30,6 +40,7 @@ function add_place($db_conn, $data) {
 
 	if ($db_conn->query($sql) === TRUE) {
 	    //echo "New place created successfully";
+	    return $db_conn->insert_id;
 	} else {
 	    echo "Error: " . $sql . "<br>" . $db_conn->error;
 	}
@@ -42,41 +53,101 @@ function add_place($db_conn, $data) {
 
 
 function add_tag($db_conn, $tag_name) {
-	$query = mysqli_query($db_conn, "SELECT * FROM tags WHERE name='" . $tag_name . "'");
+	$sql = "SELECT id FROM tags WHERE name='" . $tag_name . "'";
 
-	if (mysqli_num_rows($query) > 0){
+	$res = mysqli_query($db_conn, $sql);
+	if (mysqli_num_rows($res) > 0){
 
-	    //echo "Tag already exists";
+
+	//if ($db_conn->query($sql) === TRUE) {
+	    //echo "Place already exists";
+		$row = $res->fetch_assoc(); 
+		//$row = mysqli_fetch_array($res);
+		$id = $row['id'];
+   		//echo $id;
+		return $id;
 	} else {
 
 	$sql = "INSERT INTO tags (name) VALUES ('" . $tag_name . "')";
 
 	if ($db_conn->query($sql) === TRUE) {
 	   // echo "New tag created successfully";
+		return $db_conn->insert_id;
 	} else {
 	    echo "Error: " . $sql . "<br>" . $db_conn->error;
 	}
-	    // do something
-	    	//if (!mysqli_query($con,$query)) {
-	        //	die('Error: ' . mysqli_error($con));
-	    	//}
+	  
 	}	
+}
+function add_sum_to_place($db_conn, $place_id, $sum) {
+
+	$sql = "UPDATE places SET weight='" . $sum . "'WHERE places.place_id='" . $place_id . "'";
+	if ($db_conn->query($sql) === TRUE) {
+
+	} else {
+	    echo "Error: (add_sum) " . $sql . "<br>" . $db_conn->error;
+	    return 0;
+	}
+
+}
+
+function add_context($db_conn, $context_name) {
+	$sql = "SELECT id FROM context WHERE name='" . $context_name . "'";
+
+	$res = mysqli_query($db_conn, $sql);
+	if (mysqli_num_rows($res) > 0){
+
+
+	//if ($db_conn->query($sql) === TRUE) {
+	    //echo "Place already exists";
+		$row = $res->fetch_assoc(); 
+		//$row = mysqli_fetch_array($res);
+		$id = $row['id'];
+   		//echo $id;
+		return $id;
+	} else {
+
+	$sql = "INSERT INTO context (name) VALUES ('" . $context_name . "')";
+
+	if ($db_conn->query($sql) === TRUE) {
+
+		//$context_id = mysqli_query($db_conn, "SELECT SCOPE_IDENTITY();");
+
+	   // return $context_id;
+		 return $db_conn->insert_id;
+
+	   // echo "New tag created successfully";
+	} else {
+	    echo "Error: (add_context) " . $sql . "<br>" . $db_conn->error;
+	    return 0;
+	}
+	   
+	}	
+}
+
+
+function bind_tag_to_context($db_conn, $tag_id, $context_id, $url_count) {
+
+	$sql = "INSERT INTO mapping_context_tags (tag_id, context_id, url_count) VALUES ($tag_id, $context_id, $url_count)";
+
+	if ($db_conn->query($sql) === TRUE) {
+	    //echo "New bind created successfully";
+	} else {
+	    echo "Error (bind tag to context): " . $sql . "<br>" . $db_conn->error;
+	}
+	
+
 }
 
 function bind_tag_to_place($db_conn, $tag_id, $place_id) {
 
-	$sql = "INSERT INTO mapping (place_id, tag_id) VALUES ($place_id, $tag_id)";
+	$sql = "INSERT INTO mapping (place_id, tag_id) VALUES ('" . $place_id . "', '" . $tag_id . "')";
 
 	if ($db_conn->query($sql) === TRUE) {
-	   // echo "New bind created successfully";
+	    //echo "New bind created successfully";
 	} else {
-	    //echo "Error: " . $sql . "<br>" . $db_conn->error;
+	    echo "Error (bind tag to place): " . $sql . "<br>" . $db_conn->error;
 	}
-	    // do something
-	    	//if (!mysqli_query($con,$query)) {
-	        //	die('Error: ' . mysqli_error($con));
-	    	//}
-	//}	
 }
 
 
@@ -87,14 +158,6 @@ function get_places($db_conn) {
 	//$row = $result->fetch_array(MYSQLI_ASSOC);
 	//$result->close();
 	return $result;
-
-	//SELECT name FROM tags WHERE tags.id IN ( SELECT tag_id FROM mapping WHERE mapping.place_id = '6')
-	
-	    // do something
-	    	//if (!mysqli_query($con,$query)) {
-	        //	die('Error: ' . mysqli_error($con));
-	    	//}
-	
 }
 
 function get_places_with_title($db_conn, $title) {
@@ -104,13 +167,41 @@ function get_places_with_title($db_conn, $title) {
 	//$row = $result->fetch_array(MYSQLI_ASSOC);
 	//$result->close();
 	return $result;
+}
 
-	//SELECT name FROM tags WHERE tags.id IN ( SELECT tag_id FROM mapping WHERE mapping.place_id = '6')
-	
-	    // do something
-	    	//if (!mysqli_query($con,$query)) {
-	        //	die('Error: ' . mysqli_error($con));
-	    	//}
+function get_place_id($db_conn, $title) {
+	$query = "SELECT place_id FROM places WHERE places.title='" . $title . "';";//='" . $title . "'"; //
+
+	$result = $db_conn->query($query);
+	//$row = $result->fetch_array(MYSQLI_ASSOC);
+	//$result->close();
+	return $result;
+}
+
+
+function get_places_by_weight($db_conn, $context) {
+	$query = "SELECT places.title, places.description, places.lat, places.lon, places.weight, tags.name, context.name FROM places, tags, mapping_context_tags, mapping, context WHERE mapping.place_id=places.place_id AND tags.id=mapping.tag_id AND mapping_context_tags.tag_id=tags.id AND context.id=mapping_context_tags.context_id AND context.name='" . $context . "' GROUP BY places.weight;";//='" . $title . "'"; //
+
+
+	$res = mysqli_query($db_conn, $query);
+	//var_dump($res);
+
+	if(mysqli_num_rows($res) <= 0) {
+		echo "Ничего не найдено.";
+	}
+	else {
+		for ($row_no = $res->num_rows - 1; $row_no >= 0; $row_no--) {
+	//while($row = mysqli_fetch_array($res))
+	//{
+		$res->data_seek($row_no);
+        $row = $res->fetch_assoc();
+		echo "".$row['title']."&nbsp &nbsp";
+		echo "".$row['weight']."<br><hr>";
+
+	}
+
+	}
 	
 }
+
 ?>
--- context/xml_parser.php
+++ context/xml_parser.php
@@ -8,7 +8,7 @@ function xml_parser($xml_str) {
   $results = new SimpleXMLElement($xml_str);
 
   $count = $results->found;
-  echo "<p>count (wikimapia): " . $count . "</p>";
+  //echo "<p>count (wikimapia): " . $count . "</p>";
 
   if(!$count) {
     $none = "<p>ничего не найдено</p>";
@@ -22,20 +22,34 @@ function xml_parser($xml_str) {
 
   $db_conn = db_connect();
 
+  $results_array = array();
+
+  $context = str_replace(" ", "&", trim($_POST["context"]));
+  $context_id = add_context($db_conn, $context);
+  //$context_id = $db_conn->insert_id;
+  //$tag_id = $db_conn->insert_id;
 
   while($i < $count) {  
     $place = "places_". $i; 
+
+    
+    $each_result_array = array();
+
+
     $result_html .= "<p>";
     $point = array();
     $point_tags = array();
     //echo $xml_str->places->$place->title;
     $point["title"] = $results->places->$place->title;
     $result_html .= "Название:&nbsp&nbsp&nbsp" . $results->places->$place->title;
+    $each_result_array["title"] = $results->places->$place->title;
 
     if($results->places->$place->title != null) {
       $result_html .= "<br>"; 
 
       $result_html .= "Ссылка:&nbsp&nbsp&nbsp&nbsp" . $results->places->$place->urlhtml;
+      $each_result_array["url"] = $results->places->$place->urlhtml;
+
       $point["url"] = $results->places->$place->urlhtml;
 
       $result_html .= "<br>"; 
@@ -45,45 +59,88 @@ function xml_parser($xml_str) {
 
       if($description === null) {
         $result_html .= "нет описания";
+         //$each_result_array["description"] = "";
       }
       else {
         $result_html .= $description;
         $point["description"] = $description;
+        //$each_result_array["description"] = $description;
       }
+
       //$result_html .= $xml_str->places->$place->description;
       $result_html .= "<br>"; 
       $result_html .= "Координаты:&nbsp&nbsp&nbsp" . $results->places->$place->location->lon;
+      //$each_result_array["lon"] = $results->places->$place->location->lon;
+
       $point["lon"] = $results->places->$place->location->lon;
       //$result_html .= "<br>"; 
       $result_html .= "&nbsp" . $results->places->$place->location->lat;
+     // $each_result_array["lat"] = $results->places->$place->location->lat;
       $point["lat"] = $results->places->$place->location->lat;
       $result_html .= "<br>Тэги:&nbsp&nbsp&nbsp"; 
       
 
-
+      
       $tags = $results->places->$place->tags->tags_0->title;
       $tags = trim($tags);
+      $tags = explode(",", $tags);
       $tags = str_replace(",", "", $tags);
-      $tags = explode(" ", $tags);
+      $tags = str_replace(" ", "&", $tags);
+      
 
-      foreach ($tags as $tag) {
-         $result_html .= $tag . "\n";
-         $point_tags[] .= $tag . "";
+      $yandex_search_url_count = 0;
+      $yandex_search_url_sum = 0;
+
+
+      $point_id = add_place($db_conn, $point);
+      //$point_id = $db_conn->insert_id;
+
+      if ($point_id == null) {
+        //$point_id = get_place_id($db_conn, $point["title"]);
+         echo "null point id in bind point to tag 1";
+            die();
+
       }
 
-      $result_html .= "<br><hr></p>";
-      add_place($db_conn, $point);
-      $point_id = $db_conn->insert_id;
       $tag_ids = array();
 
-      foreach ($point_tags as $point_tag) {
-        add_tag($db_conn, $point_tag);
-        $tag_id = $db_conn->insert_id;
+
+      foreach ($tags as $tag) {
+        //$result_html .= $tag . "\n";
         
-        if ($tag_id > 0) {
+        $yandex_search_url_sum += search_in_yandex($tag, $context);
+        $tag_id = add_tag($db_conn, $tag);
+        //$tag_id = $db_conn->insert_id;
+        
+    
+         /// $point_id = get_place_id($db_conn, $point["title"]);
+
+          if ($point_id == null) {
+            echo "null point id in bind point to tag 2";
+            die();
+          }
+
           bind_tag_to_place($db_conn, $tag_id, $point_id);  
-        }
+
+          $url_count = search_in_yandex($tag, $context);
+
+          if ($tag_id == null) {
+            echo "null tag id in bind context to tag ";
+            die();
+          }
+
+          //echo "url count (in yandex): " . $url_count . "<br>";
+           if ($url_count == null) {
+            $url_count = 1;
+          }
+
+          bind_tag_to_context($db_conn, $tag_id, $context_id, $url_count);
+        
       }
+
+      add_sum_to_place($db_conn, $point_id, $yandex_search_url_sum);
+
+   
 
     }
     else {
@@ -95,8 +152,79 @@ function xml_parser($xml_str) {
   }
 
   $result_html .= "<vr>";
+  //$final_result .= "<vr>";
 
-  return $result_html;
+  //return $result_html;//$final_result; //
+  get_places_by_weight($db_conn, $context);
+
+  return $results_array;
 }
 
+function search_in_yandex($keyword, $context) {
+
+  //$keyword = trim($_POST["query"]);
+
+  //echo "keyword: " . $keyword;//$keywords = explode(",",$keywords);
+  $res = Array();
+  $user = 'nastya-pavlova-93'; // логин
+  $key = '03.180579375:33ebf55a7d6898980d6426e16a4eaf72'; // ключ Яндекс.XML
+  //$context = str_replace(" ", "&", trim($_POST["context"]));
+  
+  $result_ya_1 = array();
+  //$result_ya_2 = array();
+ 
+
+  //if (!$keyword) {
+    //  echo "no keyword";
+     // die();
+  //}
+
+  $url = "https://yandex.com/search/xml?l10n=en&user=nastya-pavlova-93&key=03.180579375:33ebf55a7d6898980d6426e16a4eaf72&page=1&query=" . $keyword . "&" . $context; 
+
+  //echo "yandex url: " . $url . "<br>";
+
+  $searched_content = file_get_contents($url);
+
+  if(!$searched_content) {
+
+      $count = 0;
+      echo "no searched content: ";
+    }
+    else {
+
+ 
+    $results = new SimpleXMLElement($searched_content);
+      $human = "found-human";
+      $count = $results->response->$human;
+
+      $count = str_replace("answers found", "", $count); 
+      $count = trim($count);
+      //echo "count: " . $count;
+
+      $count_res = array();
+      $count_res = explode(" ", $count);
+      if ($count_res[1] == "mln.") {
+        
+      $count = $count_res[0] * 1000000;
+        //$count = $count_res[0] . " 000 000";
+      } 
+      elseif($count_res[1] == "thsd.") {
+        $count = $count_res[0] * 1000;
+        //$count = $count_res[0] . " 000";
+      }
+
+    }
+
+    //  echo "<p>count: " . $count . "</p>";
+
+      /*
+      foreach ($results->response->results->grouping->group as $result_group) {
+      $result_ya_1[] .= $result_group->doc->url[0];
+      //$result_google['contents'] = $resultjson->content;
+    }
+    */
+
+    return $count;
+
+}
 ?>
